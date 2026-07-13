f68b9a3865340637c85bb68f110f5a02f10e89e9
Thomaszth <joanneszomahoun@gmail.com>
2026-07-13 20:22:46 +0100

    Build succeeded

---
+++ src/constants/eventTypes.ts
@@ -0,0 +1,12 @@
+export const EVENT_TYPES = {
+  PAGE_VISIT: 'page_visit',
+  VOIR_VEHICULE: 'voir_vehicule',
+  GALLERY_CLICK: 'gallery_click',
+  CONTACTER_WHATSAPP: 'contacter_whatsapp',
+  PARTAGER_VEHICULE: 'partager_vehicule',
+  SEARCH_QUERY: 'search_query',
+  CTA_PALMARES: 'cta_palmares',
+  FORM_STARTED: 'form_started',
+  PAGINATION_DEPTH: 'pagination_depth',
+  TIME_ON_PAGE: 'time_on_page',
+} as const;
---
+++ src/constants/pageLabels.ts
@@ -0,0 +1,6 @@
+export const PAGE_LABELS = {
+  CATALOGUE: 'catalogue',
+  PALMARES: 'palmares',
+  RECHERCHES: 'recherches',
+} as const;
+export const VOITURE_DETAIL_PATH_PREFIX = '/voitures/';
